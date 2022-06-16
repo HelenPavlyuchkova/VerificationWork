90385aafab4405b248224f250ec62d8d5a6d7673
Helen <e.xolodkova@inbox.ru>
2022-06-17 02:08:43 +0300

    Добавили программу, решающую поставленную задачу

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,36 +1,36 @@
 ﻿string[] array1 = { "Hello", "2", "world", ":-)" };
 string[] array2 = { "1234", "1567", "-2", "computer science" };
 string[] array3 = { "Russia", "Denmark", "Kazan" };
-int n = 3;//количество символов
+
 
 void Print(string[] array)
 {
     System.Console.Write("Исходный массив: [ ");
-    for (int i = 0; i < array.Length-1; i++)
+    for (int i = 0; i < array.Length - 1; i++)
     {
         Console.Write($"{array[i]}, ");
     }
-    System.Console.Write($"{array[array.Length-1]} ]");
+    System.Console.Write($"{array[array.Length - 1]} ]");
 }
 
-string[] ResArray(string[] array, int count = 0)
+string[] ResArray(string[] array, int n = 3) //n -количество символов строки массива
 {
-    string[] newarray = new string[count];
-    count = 0;
+    int count = 0;
+    string[] resarray = new string[array.Length];
     Console.Write($"Искомый массив:[ ");
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= n)
         {
-            newarray[count] = array[i];
-            Console.Write(newarray[count] + "  ");
+            resarray[count] = array[i];
+            Console.Write(resarray[count] + "  ");
             count++;
         }
     }
     Console.WriteLine("]");
-    return newarray;
+    return resarray;
 }
 
-Print(array2);
+Print(array3);
 System.Console.WriteLine();
-ResArray(array2, n);
+ResArray(array3);
